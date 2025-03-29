336f534cd4333ec19615cf1cd562e0abe97977e4
hoowk <fernandohoowk@gmail.com>
2025-03-29 20:49:05 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,22 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('motoboy-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icons/motoboy-icon.png',
-        '/icons/motoboy-icon-512.png',
-      ]);
-    })
-  );
+const CACHE_NAME = 'gomoto-v1';
+const URLS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/icons/moto.png',
+    '/icons/1moto.png',
+    'https://api.maptiler.com/maps/streets-v2/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+    'https://unpkg.com/maplibre-gl@2.4.0/dist/maplibre-gl.css',
+    'https://unpkg.com/maplibre-gl@2.4.0/dist/maplibre-gl.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
