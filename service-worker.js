309859278a02b34b974bc024783530d5fb923ae6
hoowk <fernandohoowk@gmail.com>
2025-03-31 16:57:01 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,18 @@
-const CACHE_NAME = 'gomoto-v3';
+const CACHE_NAME = 'gomoto-v1';
 const ASSETS = [
   './',
   './index.html',
   './login.html',
-  './install.js',
   './icons/logomoto-192x192.png',
   './icons/moto.png',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
+  'https://unpkg.com/maplibre-gl@2.4.0/dist/maplibre-gl.css',
+  'https://unpkg.com/maplibre-gl@2.4.0/dist/maplibre-gl.js'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
   );
 });
 
@@ -34,12 +21,4 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request)
       .then(res => res || fetch(e.request))
   );
-});
-
-self.addEventListener('message', (e) => {
-  if (e.data.type === 'UPDATE_LOCATION') {
-    const { nome, position } = e.data.data;
-    // Simulação de envio para backend
-    console.log(`📍 ${nome}: ${position.lat}, ${position.lng}`);
-  }
 });
